e1ba332eca6f3a2dc53f44cb96d53a85f743a6d7
karta2341940 <a08487a@gmail.com>
2022-05-17 11:52:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,58 +1,118 @@
+import { link } from "fs";
+import axios from "axios";
 import { Cluster } from "puppeteer-cluster";
 
-async function classify(url = "", data = { "href": [], "title": [] }) {
-  let exteralLink = [];
-  let interalLink = [];
-  
-  if (url.endsWith("/", url.length)) {
-    url = url.substring(0, url.length - 1);
-  }
-  
-  data.href.forEach( (i,index)=>{
-    if (!i.includes(url)) {
-      let u = ["/app", "/p", "/var", "/index.php"];
-      for (let j of u) {
-
-        if (i.startsWith(j, 0)) {
-          i = i.replace(j, url + j)
-        }
-      }
-    }
-    if (i.startsWith(url, 0)) {
-      console.log(i)
-      interalLink.push(i);
-    }
-    else {
-      exteralLink.push(i);
-    }
-  });
-  
-
-}
+let output={}
 
 
-(async () => {
-  const cluster = await Cluster.launch({
+let cluster = Cluster.launch({
+  concurrency: Cluster.CONCURRENCY_CONTEXT,
+  maxConcurrency: 10,
+});
+
+async function checkLayer2(originUrl='',urlArray=[]){
+  let cluster2 = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
     maxConcurrency: 10,
   });
 
-  await cluster.task(async ({ page, data: url }) => {
+  await cluster2.task(async ({ page, data: url }) => {
     await page.goto(url);
-    const screen = await page.screenshot();
-    let data = await page.evaluate(() => {
+    let data2 = await page.evaluate(() => {
       let href = Array.from(document.querySelectorAll('a'), a => a.getAttribute('href'));
       let content = Array.from(document.querySelectorAll('a'), a => a.textContent);
-      return {"href": href,"title": content}
+      return { "href": href, "title": content }
     });
+    let data = await classify(url, data2);
+    
+    for(let i of data.interalLink.href){
+      await checkURL(i,url);
+    }
 
-    //classify(url, data);
+
+
+
+  });
+  
+}
+
+async function classify(url = "", data = { "href": [], "title": [] }) {
+  let totalLink = { "href": [], "title": [] }
+  let exteralLink = { "href": [], "title": [] };
+  let interalLink = { "href": [], "title": [] };
+
+  if (url.endsWith("/", url.length)) {
+    url = url.substring(0, url.length - 1);
+  }
+  for (let i in data.href) {
+    
+    if (String(data.href[i]).startsWith('/index.php')) continue;
+
+    if (!data.href[i]) continue;
+    
+    if(data.href[i].includes('#'))continue;
+
+    if (!data.href[i].includes(url)) {
+      
+        if (data.href[i].startsWith('/', 0)) {
+          data.href[i] = data.href[i].replace('/', url + '/')
+        }
+      
+    }
+
+    if (data.href[i].startsWith(url, 0)) {
+      interalLink.href.push(data.href[i]);
+      interalLink.title.push(data.title[i]);
+    }
+    else {
+      exteralLink.href.push(data.href[i]);
+      exteralLink.title.push(data.title[i]);
+    }
+    totalLink.href.push(data.href[i]);
+    totalLink.title.push(data.title[i]);
+  }
+
+  return { interalLink, exteralLink,totalLink}
+
+}
+
+async function checkURL(url="",orgURL){
+  
+  let resp = await axios.get(url,{beforeRedirect:()=>{
+
+  }});
+
+  console.log(`${url} : `,resp.status)
+  
+}
+
+(async () => {
+
+  cluster = await cluster;
+  await cluster.task(async ({ page, data: url }) => {
+    await page.goto(url);
+    let data2 = await page.evaluate(() => {
+      let href = Array.from(document.querySelectorAll('a'), a => a.getAttribute('href'));
+      let content = Array.from(document.querySelectorAll('a'), a => a.textContent);
+      return { "href": href, "title": content }
+    });
+    let data = await classify(url, data2);
+    //console.log(data.exteralLink.href);
+
+    for(let i of data.interalLink.href){
+      checkURL(i,url);
+      
+    }
+
+
+
 
   });
 
 
   cluster.queue('https://www.nptu.edu.tw/');
-  //cluster.queue('http://www.wikipedia.org/');
+
+
   // many more pages
 
   await cluster.idle();
